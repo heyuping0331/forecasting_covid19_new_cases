3b670fce709f8314fcd27f813a39e60f392b1f2a
Yuping He <yupingheee@gmail.com>
2020-06-03 14:02:23 -0700

    updating

--- covid19prophet/fbprophet.py
+++ covid19prophet/fbprophet.py
@@ -10,7 +10,8 @@ from fbprophet import Prophet
 from fbprophet.plot import add_changepoints_to_plot, plot_cross_validation_metric
 from fbprophet.diagnostics import cross_validation, performance_metrics
 
-def fit_model(data, trend_func, changepoint_prior, changepoint_range, weekly_fourier_order):
+# add conditional seasonality
+def fit_model(data, trend_func, changepoint_prior, changepoint_range, weekly_fourier_order, seasonal_mode):
     '''
 
     Output --> FB Prophet object
@@ -21,11 +22,12 @@ def fit_model(data, trend_func, changepoint_prior, changepoint_range, weekly_fou
         ,yearly_seasonality = False
         ,daily_seasonality = False
         ,changepoint_prior_scale = changepoint_prior
-        ,changepoint_range = changepoint_range)
+        ,changepoint_range = changepoint_range
+        ,seasonality_mode = seasonal_mode)
 
     if trend_func == 'logistic':
         data['cap'] = 12000
-        data['floor'] = 0
+        data['floor'] = 10
 
     return m.fit(data)
 
@@ -54,12 +56,18 @@ def tune_hyperparameters(data, model_prophet, model_cv, grid):
     df2 = pd.DataFrame({'changepoint_prior': grid['changepoint_prior']})
     df3 = pd.DataFrame({'changepoint_range': grid['changepoint_range']})
     df4 = pd.DataFrame({'weekly_fourier_order': grid['weekly_fourier_order']})
-    df_param = df1.assign(foo=1).merge(df2.assign(foo=1)).merge(df3.assign(foo=1)).merge(df4.assign(foo=1)).drop('foo', 1)
+    df5 = pd.DataFrame({'seasonal_mode': grid['seasonal_mode']})
+    df_param = df1.assign(foo=1).merge(df2.assign(foo=1)).merge(df3.assign(foo=1)).merge(df4.assign(foo=1)).merge(df5.assign(foo=1)).drop('foo', 1)
     df_param['MAPE'] = None
 
-    # get MAPE for each run
+    # get MAPE for each parameter set
     for row in range(len(df_param)):       
-        model = model_prophet(data, df_param.loc[row, 'growth'], df_param.loc[row, 'changepoint_prior'], df_param.loc[row, 'changepoint_range'], df_param.loc[row,'weekly_fourier_order'])
+        model = model_prophet(data, 
+            df_param.loc[row, 'growth'], 
+            df_param.loc[row, 'changepoint_prior'], 
+            df_param.loc[row, 'changepoint_range'], 
+            df_param.loc[row, 'weekly_fourier_order'], 
+            df_param.loc[row, 'seasonal_mode'])
         mape = model_cv(model, '21 days', '7 days', '7 days')
 
         df_param.loc[row,'MAPE'] = mape
@@ -67,7 +75,8 @@ def tune_hyperparameters(data, model_prophet, model_cv, grid):
     return df_param.sort_values('MAPE')
 
 
-def make_forecasts(model_prophet, best_trend_func, best_changepoint_prior, best_changepoint_range, best_weekly_fourier_order, ts, horizon):
+def make_forecasts(model_prophet, ts, horizon,
+    best_trend_func, best_changepoint_prior, best_changepoint_range, best_weekly_fourier_order, best_seasonal_mode):
     '''
 
     Output --> final_model, final_forecasts
@@ -79,7 +88,8 @@ def make_forecasts(model_prophet, best_trend_func, best_changepoint_prior, best_
         trend_func = best_trend_func, 
         changepoint_prior = best_changepoint_prior, 
         changepoint_range = best_changepoint_range, 
-        weekly_fourier_order = best_weekly_fourier_order)
+        weekly_fourier_order = best_weekly_fourier_order,
+        seasonal_mode = best_seasonal_mode)
     
     # create future dates
     ts_future = model.make_future_dataframe(periods = horizon)
--- covid19prophet/sarima.py
+++ covid19prophet/sarima.py
@@ -1,17 +1,56 @@
 
 
-# fit model with params
+
+import pandas as pd
+import numpy as np
+import itertools
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+
+# grid search --> walk-forward-validated mape
+def walk_forward_validation(p,q,d,P,Q,D,s, freq, training_range, forecast_horizon, data):
+
+  # make grid
+  pdq = list(itertools.product(p, d, q, P, D, Q, s))
+  df_params = pd.DataFrame(pdq)
+  df_params.columns = ['p','d','q','P','D','Q', 's']
+  df_params['mape'] = None
+
+  # starting day for each validation set
+  day_list = []
+  i=0
+  while (len(data)-freq*i>=training_range):
+      day_list.append(freq*i)
+      i += 1
+
+  # iterate over each hyperparameter combination
+  for index, row in df_params.iterrows():
+
+      mape_list = []
+      # walk-forward validation
+      for day in [0, freq, freq*2, freq*3, freq*4]:
+          # fit model
+          model = SARIMAX(data.iloc[day:day+training_range,:], order = (row['p'],row['d'],row['q']), seasonal_order = (row['P'],row['D'],row['Q'],row['s']))
+          results = model.fit(max_iter = 50, method = 'powell')
+          # dynamic forecast (not one-step-ahead forecast)
+          pred = results.get_forecast(steps = forecast_horizon, dynamic=True).predicted_mean.reset_index()
+          y = data.iloc[day+training_range:day+training_range+forecast_horizon,:].reset_index()
+          temp = pd.merge(pred, y, how='inner', left_on='index', right_on='Date')
+          # avg mape over 7-day horizon
+          mape = np.mean(np.abs(temp[0] - temp['Cases_new'])/temp['Cases_new'])
+          # n validation sets
+          mape_list.append(mape)
+      
+      # avg avg mape over 5 validation sets
+      df_params.loc[index, 'mape'] = np.mean(mape_list)
+
+  return df_params.sort_values('mape')
 
 
 
-# evaluation/cross-validate/performance metrics
 
 
 
-# model diagnostics
 
 
-
-# tune hyperparameters???
 
 
